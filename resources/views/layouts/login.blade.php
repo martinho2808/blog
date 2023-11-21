09c273b9924890509df63560a3a886fc1e571377
martin <kit.hmk@gmail.com>
2023-11-22 01:59:39 +0800

    fix login bar css

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -3,27 +3,34 @@
         @if (Route::has('login'))
             <div class="row">
                 
-                <div class="col-md-6 col-sm-6 text-left fh5co-link">
+                <div class="col-md-6 col-sm-6 text-left fh5co-link" style="color:white;">
+                @auth
+                Hello! {{ Auth::user()->name }}
+                @endauth
                 </div>
                     @auth
-                       
-
-                        <div class="col-md-6 col-sm-6 text-right fh5co-social">
-                            <div>{{ Auth::user()->name }}</div>
-                            <a href="{{route('profile.edit')}}">
-                                {{ __('Profile') }}
-                            </a>
-
+                        
+                    
+                        <div class="col-md-6 col-sm-6 text-right fh5co-social login-nav">                       
                             <!-- Authentication -->
+                           
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-
+                                
+                                <a href="{{route('profile.edit')}}">{{ __('Profile') }}</a>
+                                @if(Auth::user()->Role =='Admin' )
+                                <a href="{{route('dashboard')}}">
+                                    {{ __('Management') }}
+                                </a>
+                                @endif
                                 <a :href="route('logout')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
                                     {{ __('Log Out') }}
                                 </a>
+                               
                             </form>
+                            </ul>
                         </div>          
                     @else
                         <div class="col-md-6 col-sm-6 text-right fh5co-social">
